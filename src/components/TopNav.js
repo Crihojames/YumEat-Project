a0f298a70d4850d10d35d276b482c0a2ef83e6b3
Crihojames <crihojames@gmail.com>
2023-11-25 11:42:03 +0100

    add  update

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,14 +1,37 @@
 import React from "react"
-import {AiOutlineMenu, AiOutlineAlignRight, AiOutlineClose, AiFillTag} from "react-icons/ai"
+import {
+  AiOutlineMenu,
+  AiOutlineAlignRight,
+  AiOutlineClose,
+  AiFillTag,
+  AiOutlineSearch,
+}
+import {BsFillCarFill, BsPerson} from "react-icons/bs"
+from "react-icons/ai"
 
 const TopNav = () => {
   return (
-    <div className="max-w-[1520] mx-auto flex justify-between items-center p-4">
+    <div className="max-w-[1520px] mx-auto flex justify-between items-center p-4">
       <div className="flex items-center">
         <div className="cursor-pointer">
-            <AiOutlineMenu size={25} />
+          <AiOutlineMenu size={25} />
+        </div>
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
+          Yum<span>Eats</span>
+        </h1>
+        <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
+          <p className="bg-orange-700 text-white rounded-full p-2 font-bold">Free</p>
+          <p className="p-2 font-bold ">Delivery</p>
         </div>
       </div>
+
+      <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+        <AiOutlineSearch size={25}/>
+        <input className="bg-transparent p-2 w-full focus:outline-none"
+            type='text' placeholder='search meals'
+        />
+      </div>
+       
     </div>
   )
 }
