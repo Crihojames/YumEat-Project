bda8d657762a55cbd257e2d2d6f2e0edd80c3c69
Crihojames <crihojames@gmail.com>
2023-11-27 16:52:42 +0100

    add sideNav Menu design

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -3,11 +3,13 @@ import {
   AiOutlineMenu,
   AiOutlineAlignRight,
   AiOutlineClose,
-  AiFillTag,
   AiOutlineSearch,
 }from "react-icons/ai"
+import {BsFillCarFill, BsPerson} from 'react-icons/bs';
+import {TbTruckReturn} from 'react-icons/tb'
+import {FaUserFriends, FaGoogleWallet} from 'react-icons/fa'
+import {MdHelp, MdOutlineFavorite} from 'react-icons/md'
 
-// import {BsFillCarFill} from 'react-icons/bs'
 
 const TopNav = () => {
   const [sideNav, setSideNav] = useState(false)
@@ -40,17 +42,60 @@ const TopNav = () => {
         ): ("")
        }
 
+
+
        <div className={sideNav 
        ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300"
        : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
        }
        >
-        <AiOutlineClose onClick={()=> setSideNav(!sideNav)} size={25}/>
+        <AiOutlineClose onClick={()=> setSideNav(!sideNav)} size={25}
+          className="absolute  right-4 top-4 cursor-pointer"
+        />
+        <h2 className="text-2xl p-4">Yum <span className="text-orange-700 font-bold">Eats</span></h2>
+          <nav>
+            <ul className="flex flex-col p-4 text-gray-900">
+              
+              <li className="text-xl py-4 flex">
+                <BsPerson size={25}
+                  className="mr-4 text-white br-black rounded-full"
+                />
+                My Account
+              </li>
 
+              <li className="text-xl py-4 flex">
+                <TbTruckReturn size={25}
+                  className="mr-4 text-white br-black rounded-full"
+                />
+                Delivery
+              </li>
+
+              <li className="text-xl py-4 flex">
+                <MdOutlineFavorite size={25}
+                  className="mr-4 text-white br-black rounded-full"
+                />
+                My Favourite
+              </li>
+
+              <li className="text-xl py-4 flex">
+                <FaGoogleWallet size={25}
+                  className="mr-4 text-white br-black rounded-full"
+                />
+                My wallet
+              </li>
+
+              <li className="text-xl py-4 flex">
+                <MdHelp size={25}
+                  className="mr-4 text-white br-black rounded-full"
+                />
+                Help
+              </li>
+            </ul>
+          </nav>
        </div>
        
     </div>
   )
 }
 
-export default TopNav
+export default TopNav;
