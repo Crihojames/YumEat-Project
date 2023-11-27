5262f0c48f3558662d0daead990e24bab90c3ef9
Crihojames <crihojames@gmail.com>
2023-11-27 17:04:21 +0100

    SideNav Final Touch

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -21,7 +21,7 @@ const TopNav = () => {
           <AiOutlineMenu size={25} />
         </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
-          Yum<span>Eats</span>
+          Yum<span className="font-bold">Eats</span>
         </h1>
         <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
           <p className="bg-orange-700 text-white rounded-full p-2 font-bold">Free</p>
@@ -38,7 +38,8 @@ const TopNav = () => {
        <button className="bg-orange-700 text-white hidden md:flex items-center py-2 rounded-full ">Cart</button>
        {
         sideNav ? (
-          <div className="bg-black/60 fixed w-full h-screen z-10 top-0 left-0"></div>
+          <div className="bg-black/60 fixed w-full h-screen z-10 top-0 left-0"
+          onClick={()=> setSideNav(!sideNav)}></div>
         ): ("")
        }
 
