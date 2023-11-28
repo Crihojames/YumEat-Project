d1565d493f6dda52e0f11d423c3d8ff4da4a8fb8
Crihojames <crihojames@gmail.com>
2023-11-28 18:09:44 +0100

    add fonctionality of the butons next en prev

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -9,17 +9,30 @@ const Featured = () => {
     ]
     
     const [currentIndex, setCurrentIndex] = useState(0)
+
+        const prevSlide = ()=>{
+            const isFirstSlide = currentIndex === 0
+            const newIndex = isFirstSlide ? sliders.length - 1 : currentIndex -1
+            setCurrentIndex(newIndex)
+        }
+
+        const nextSlider = ()=>{
+            const isFirstSlide = currentIndex === sliders.length - 1
+            const newIndex = isFirstSlide ? 0: currentIndex +1
+            setCurrentIndex(newIndex)
+        }
+
     return (
-        <div className=' max-w-[2520px] h-[500px] w-full py-4 px-4 relative'>
+        <div className=' max-w-[2520px] h-[500px] w-full py-4 px-4 relative group'>
             <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
             style={{backgroundImage: `url(${sliders[currentIndex].url})`}} 
             ></div>
 
-            <div className='absolute  top-[50%] -translate-x-0 translate-y[-50%] left-5 text-2xl rounded-full p-2  bg-orange-700 text-white cursor-pointer'>
-            <BsChevronCompactLeft/>
+            <div className='hidden group-hover:block absolute  top-[50%] -translate-x-0 translate-y[-50%] left-5 text-2xl rounded-full p-2  bg-orange-700 text-white cursor-pointer'>
+            <BsChevronCompactLeft onClick={prevSlide}/>
             </div>
-            <div className='absolute  top-[50%] -translate-x-0 translate-y[-50%] right-5 text-2xl rounded-full p-2  bg-orange-700 text-white cursor-pointer'>
-            <BsChevronCompactRight/>
+            <div className='hidden group-hover:block absolute  top-[50%] -translate-x-0 translate-y[-50%] right-5 text-2xl rounded-full p-2  bg-orange-700 text-white cursor-pointer'>
+            <BsChevronCompactRight onClick={nextSlider}/>
             </div>
             
         </div>
