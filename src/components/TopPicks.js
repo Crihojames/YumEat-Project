ee784b86a36e4902a4fb1c107e1b45d9291850f7
Crihojames <crihojames@gmail.com>
2023-12-02 19:12:56 +0100

    top picks add to the price end title

--- src/components/TopPicks.js
+++ src/components/TopPicks.js
@@ -8,20 +8,25 @@ const TopPicks = () => {
         <>
             <h1 className='text-orange-500 font-bold text-2xl text-center py-2'>Top Picks</h1>
             <div className='hidden lg:flex max-w[1520px] m-auto py-2 px-2'>
+            <Splide options={{perPage: 4, gap: "0.5rem", grap: 'free', arrows: false}}>
                 {
                     topPicks.map((item) => {
                         return(
-                            <div className='rounded-3xl relative'>
-                                <div className='absolute w-full h-full bg-black/50 rounded-3xl text-white'>
-                                    <p className='px-2'>{item.title}</p>
-                                    <button className='border-dotted border-white text-white mx-2 absolute bottom-4'>Add To Cart</button>
+                            <SplideSlide key={item.id}>
+                                <div className='rounded-3xl relative'>
+                                    <div className='absolute w-full h-full bg-black/50 rounded-3xl text-white'>
+                                        <p className='px-2 pt-4 font-bold text-2xl'>{item.title}</p>
+                                        <p className='px-2'>{item.price}</p>
+                                        <button className='border-dotted border-white text-white mx-2 absolute bottom-4'>Add To Cart</button>
+                                    </div>
+                                    <img src={item.img}  alt={item.title}  className='h-[200px] w-full object-cover rounded-3xl cursor-pointer hover:scale-105 ease-out duration-300'/>
                                 </div>
-                                <img src={item.img} alt=""  className='h-[200px] w-full object-cover rounded-3xl cursor-pointer hover:scale-105 ease-out duration-300'/>
-                            </div>
+                            </SplideSlide>
                         )
                     })
 
                 }
+            </Splide>
             </div>
         </>
     );
